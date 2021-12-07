b4eb8dae459e0e46cbb314070a320c9b15898ff7
98pragya <210251@softwarica.edu.np>
2021-12-07 21:50:07 +0545

    Initial commit

---
+++ srs/PyramidStar.java
@@ -0,0 +1,21 @@
+//*5.c.Pyramid Star Pattern
+import java.util.Scanner;
+
+public class PyramidStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("enter the number of row: ");
+        int row = sc.nextInt();
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
